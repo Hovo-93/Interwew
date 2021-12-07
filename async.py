23fd789aceeb0a946ca421339e216f4dadefc0bc
Hovo-93 <ovo.aroyan@mail.ru>
2021-12-07 22:26:29 +0300

    added async

--- async.py
+++ async.py
@@ -1,16 +1,23 @@
-import gevent
-from urllib.request import urlopen
+import asyncio
+import aiohttp
 
 URLS = ['http://127.0.0.1:8000/product/1/', 'http://127.0.0.1:8000/product/2/']
 
 
-def get_data(URL):
-    data = urlopen(URL).read()
-    file = open("otus.txt", "w")
-    file.write(f'{data}')
-    file.close()
-    print(f'resp:{len(data)}')
+async def main():
+    async with aiohttp.ClientSession() as session:
+        for url in URLS:
+            print(url)
+            async with session.get(url) as response:
+                print("Status:", response.status)
+                print("Content-type:", response.headers['content-type'])
+
+                html = await response.text()
+                file = open("data.txt", "w")
+                file.write(f'{html}')
+                file.close()
+                print("Body:", html[:15], "...")
 
 
-prod = [gevent.spawn(get_data, u) for u in URLS]
-gevent.wait(prod)
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
